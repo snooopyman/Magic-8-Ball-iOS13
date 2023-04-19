043c4899028dae326fc9d29d5f305c01d5906019
Snooopyman <armando.snoop@gmail.com>
2023-04-19 23:39:41 +0200

    creating Magic Ball App.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var imageToChange: UIImageView!
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
 
-
+    @IBAction func rollButtonPresed(_ sender: Any) {
+        imageToChange.image = ballArray.randomElement()
+    }
 }
 
